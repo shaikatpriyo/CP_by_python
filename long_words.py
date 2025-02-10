1b25f8ac86c0099ab042fd2ae5bbe294852575ef
Shaikat <sshaikat.ku@gmail.com>
2025-02-10 11:52:54 +0600

    Add long words again

--- long_words.py
+++ long_words.py
@@ -3,7 +3,7 @@ n=int(input())
 for _ in range (n):
     word=input()
 if len(word)>10 :
-    print(f"{word[0]}{len(word) -2}{word[-1]}")
+    print(word[0]+ str(len(word) -2)+ word[-1])
 else :
     print(word)
 
