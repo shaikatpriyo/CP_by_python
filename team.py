ffb5e6a9e1edc20024196ac767112c04a534bd80
Shaikat <sshaikat.ku@gmail.com>
2025-02-13 21:46:31 +0600

    Add new team problem

--- team.py
+++ team.py
@@ -1,17 +1,17 @@
-# Read the number of problems
+
 n = int(input())
 
-# Initialize a counter for the number of problems they will solve
+
 count = 0
 
-# Iterate through each problem
+
 for _ in range(n):
-    # Read the three integers for the current problem
+    
     a, b, c = map(int, input().split())
     
-    # Check if at least two of them are sure about the solution
+   
     if a + b + c >= 2:
         count += 1
 
-# Output the result
+
 print(count)
