3f48451055d48ae96cdbd6426783107777fade5c
Shaikat <sshaikat.ku@gmail.com>
2025-02-14 22:11:30 +0600

    Add bit new

--- Bit.py
+++ Bit.py
@@ -1,10 +1,10 @@
-# Read the number of statements
+
 n = int(input())
 
-# Initialize x to 0
+
 x = 0
 
-# Process each statement
+
 for _ in range(n):
     statement = input().strip()
     if "++" in statement:
@@ -12,5 +12,5 @@ for _ in range(n):
     elif "--" in statement:
         x -= 1
 
-# Output the final value of x
+
 print(x)
