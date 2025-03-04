6f69d8a4b0c92801b2562016e6fadd4c512be12c
Shaikat <sshaikat.ku@gmail.com>
2025-03-04 23:52:34 +0600

    Add Chess

---
+++ Chess.py
@@ -0,0 +1,16 @@
+def find_winner(n, s):
+    a_wins = s.count('A')
+    d_wins = s.count('D')
+    
+    if a_wins > d_wins:
+        print("Anton")
+    elif d_wins > a_wins:
+        print("Danik")
+    else:
+        print("Friendship")
+
+
+n = int(input().strip())
+s = input().strip()
+
+find_winner(n, s)
